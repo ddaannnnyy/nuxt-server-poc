0e956c3fbb901d836cc8d281572df5e0b1684d1e
ddaannnnyy <32764014+ddaannnnyy@users.noreply.github.com>
2024-11-26 16:26:38 +1000

    added get artist post

---
+++ server/api/spotify/getArtist.post.ts
@@ -0,0 +1,121 @@
+import type { SpotifyCredentialResponse, SpotifyQueue } from "~~/types";
+import { FetchError, FetchResponse } from "ofetch";
+
+// Default Spotify Token Values
+const spotifyVariables = {
+  access: useRuntimeConfig().spotify.accessToken,
+  refresh: useRuntimeConfig().spotify.refreshToken,
+  auth: useRuntimeConfig().spotify.authToken,
+  apiBase: useRuntimeConfig().spotify.apiBase,
+  accountURL: useRuntimeConfig().spotify.accountBase,
+};
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  if (!body.id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Bad Request",
+      message: "Unable to read ID",
+      cause: `Unable to read id from body`,
+    });
+  }
+  async function getArtist() {
+    const url = `${spotifyVariables.apiBase}/artists/${body.id}`;
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + spotifyVariables.access,
+    };
+    try {
+      const data: FetchResponse<SpotifyQueue> = await $fetch(url, {
+        method: "GET",
+        headers,
+      });
+      return data._data?.queue;
+    } catch (error: FetchError | unknown) {
+      if (error instanceof FetchError) {
+        if (error.statusCode === 401) {
+          console.warn("Spotify Credentials are invalid");
+          const tokens: void = await refreshToken();
+        } else {
+          throw createError({
+            statusCode: error.statusCode,
+            statusMessage: error.statusMessage,
+            cause: error.cause,
+            message: error.message,
+          });
+        }
+      } else {
+        throw createError({
+          statusCode: 500,
+          statusMessage: "Internal Server Error",
+          message: "Unable to fetch Queue",
+          cause: `getQueue error is not type of FetchError, and so cannot be parsed. Dumping error\r\n---\r\n${error}`,
+        });
+      }
+    }
+  }
+
+  /**
+   * Uses stored refresh token to renew the Spotify access token
+   *
+   * Tokens last 3600s (1 hour)
+   */
+  async function refreshToken() {
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${spotifyVariables.auth}`,
+    };
+    // TODO this can probably be put into the $fetch directly
+    const request = new URLSearchParams();
+    request.append("grant_type", "refresh_token");
+    request.append("refresh_token", spotifyVariables.refresh);
+
+    try {
+      const data: FetchResponse<SpotifyCredentialResponse> = await $fetch(
+        spotifyVariables.accountURL,
+        {
+          method: "POST",
+          body: request,
+          headers,
+        }
+      );
+
+      if (data._data && data._data.access_token) {
+        spotifyVariables.access = data._data.access_token;
+      } else {
+        throw createError({
+          statusCode: 500,
+          statusMessage: "Internal Server Error",
+          message: "Unable to fetch Queue",
+          cause: `getQueue/refreshToken error is not type of FetchError, and so cannot be parsed. Dumping error\r\n---\r\naccess_token does not exist on data response`,
+        });
+      }
+    } catch (error: FetchError | unknown) {
+      if (error instanceof FetchError) {
+        throw createError({
+          statusCode: error.statusCode,
+          statusMessage: error.statusMessage,
+          message: error.message,
+          cause: error.cause,
+        });
+      } else {
+        console.error(error);
+        throw createError({
+          statusCode: 500,
+          statusMessage: "Internal Server Error",
+          message: "Unable to fetch Queue",
+          cause: `getQueue/refreshToken error is not type of FetchError, and so cannot be parsed. Dumping error\r\n---\r\n${error}`,
+        });
+      }
+    }
+  }
+
+  // API runs from here
+  const artist = await getArtist();
+
+  return {
+    artist: artist,
+  };
+});
